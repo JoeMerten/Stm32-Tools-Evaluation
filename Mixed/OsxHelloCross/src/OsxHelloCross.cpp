e9fd061f48f50501606bc237c6773c2b6c28d345
Joe Merten <joe.merten@numberfour.eu>
2015-11-23 08:45:38 +0100

    Testing with some Osx eclipse project setups

---
+++ Mixed/OsxHelloCross/src/OsxHelloCross.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OsxHelloCross.cpp
+// Author      : Joe
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
